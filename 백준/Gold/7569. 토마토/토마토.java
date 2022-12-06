ea93f2bb358ecf393f45a52c8b52ca510f630ad5
imbeom35 <97426151+imbeom35@users.noreply.github.com>
2022-12-06 10:52:05 +0900

    [Gold V] Title: 토마토, Time: 760 ms, Memory: 120776 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,6 +1,17 @@
 import java.io.*;
 import java.util.*;
 
+class tomato{
+	int x;
+	int y;
+	int z;
+	tomato(int z, int x, int y){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
+
 public class Main {
 	public static int count = 0;
 	
@@ -8,8 +19,8 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken()); //가로
+		int N = Integer.parseInt(st.nextToken()); //세로
 		int H = Integer.parseInt(st.nextToken());
 		int[][][] box = new int[H][N][M];
 		
@@ -22,33 +33,27 @@ public class Main {
 			}
 		}
 		
-		for(int i=0; i<H; i++) {
-			for(int j=0; j<N; j++) {
-				for(int k=0; k<M; k++) {
-					
-				}
-			}
-		}
-		
 		System.out.println(Ripen(box));
 		
 		br.close();
 	}
 	
 	public static int Ripen(int[][][] arr) {
-		Queue<Integer> queue = new LinkedList<Integer>();
+		Queue<tomato> queue = new LinkedList<tomato>();
 		int count = 0;
 		int buf1 = 0;
 		int buf2 = 0;
 		boolean flag = false;
 		
+		int[] dx = {1, -1, 0, 0, 0, 0};
+		int[] dy = {0, 0, 1, -1, 0, 0};
+		int[] dz = {0, 0, 0, 0, 1, -1};
+		
 		for(int i=0; i<arr.length; i++) {
 			for(int j=0; j<arr[0].length; j++) {
 				for(int k=0; k<arr[0][0].length; k++) {
 					if(arr[i][j][k] == 1) {
-						queue.add(i);
-						queue.add(j);
-						queue.add(k);
+						queue.add(new tomato(i, j, k));
 						buf2++;
 					}
 					
@@ -64,71 +69,23 @@ public class Main {
 		}
 		
 		while(!queue.isEmpty()) {
-			int x = queue.poll();
-			int y = queue.poll();
-			int z = queue.poll();
+			tomato t = queue.poll();
 			
-			//System.out.println(buf2 + " " + x + " " + y + " " + z);
+			int x = t.x;
+			int y = t.y;
+			int z = t.z;
 			
-			try {
-				if(arr[x+1][y][z] == 0) {
-					queue.add(x+1);
-					queue.add(y);
-					queue.add(z);
-					arr[x+1][y][z] = 1;
-					buf1++;
-				}
-			}catch(ArrayIndexOutOfBoundsException e) {}
+			//System.out.println(x + " " + y + " " + z);
 			
-			try {
-				if(arr[x-1][y][z] == 0) {
-					queue.add(x-1);
-					queue.add(y);
-					queue.add(z);
-					arr[x-1][y][z] = 1;
-					buf1++;
-				}
-			}catch(ArrayIndexOutOfBoundsException e) {}
-			
-			try {
-				if(arr[x][y+1][z] == 0) {
-					queue.add(x);
-					queue.add(y+1);
-					queue.add(z);
-					arr[x][y+1][z] = 1;
-					buf1++;
-				}
-			}catch(ArrayIndexOutOfBoundsException e) {}
-			
-			try {
-				if(arr[x][y-1][z] == 0) {
-					queue.add(x);
-					queue.add(y-1);
-					queue.add(z);
-					arr[x][y-1][z] = 1;
-					buf1++;
-				}
-			}catch(ArrayIndexOutOfBoundsException e) {}
-			
-			try {
-				if(arr[x][y][z+1] == 0) {
-					queue.add(x);
-					queue.add(y);
-					queue.add(z+1);
-					arr[x][y][z+1] = 1;
-					buf1++;
-				}
-			}catch(ArrayIndexOutOfBoundsException e) {}
-			
-			try {
-				if(arr[x][y][z-1] == 0) {
-					queue.add(x);
-					queue.add(y);
-					queue.add(z-1);
-					arr[x][y][z-1] = 1;
-					buf1++;
-				}
-			}catch(ArrayIndexOutOfBoundsException e) {}
+			for(int i=0; i<6; i++) {
+				try {
+					if(arr[z+dz[i]][x+dx[i]][y+dy[i]] == 0) {
+						queue.add(new tomato(z+dz[i], x+dx[i], y+dy[i]));
+						arr[z+dz[i]][x+dx[i]][y+dy[i]] = 1;
+						buf1++;
+					}
+				}catch(ArrayIndexOutOfBoundsException e) {}
+			}
 			
 			if(buf2 > 0) {
 				buf2--;
@@ -138,7 +95,7 @@ public class Main {
 				buf2 = buf1;
 				buf1 = 0;
 				count++;
-				//System.out.println("-------");
+				//System.out.println("-----");
 			}
 		}
 		
