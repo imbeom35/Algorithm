00852336689c09d736e19f84efd5720d4b4a501b
imbeom35 <97426151+imbeom35@users.noreply.github.com>
2023-03-14 22:42:42 +0900

    Update 최소비용 구하기 2.java

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -2,84 +2,90 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class City implements Comparable<City> {
-		int to;
-		int weight;
-
-		public City(int to, int weight) {
-			this.to = to;
-			this.weight = weight;
+	
+	private static class Edge implements Comparable<Edge>{
+		int index;
+		int cost;
+		
+		public Edge(int index, int cost) {
+			this.index = index;
+			this.cost = cost;
 		}
 
 		@Override
-		public int compareTo(City o) {
-			return this.weight - o.weight;
+		public int compareTo(Edge o) {
+			return this.cost - o.cost;
 		}
 	}
-
-	static int N, M;
-	static int d[], preCity[];
-	static int start, end;
-	static List<ArrayList<City>> graph;
-
+	
+	private static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
+	private static int n, m;
+	private static int[] dist, preCity;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-
-		d = new int[N + 1];
-		preCity = new int[N + 1];
-		Arrays.fill(d, Integer.MAX_VALUE);
-
-		graph = new ArrayList<>();
-		for (int i = 0; i < N + 1; i++)
-			graph.add(new ArrayList<City>());
-
-		for (int i = 0; i < M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			graph.get(from).add(new City(to, weight));
-
-		}
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		start = Integer.parseInt(st.nextToken());
-		end = Integer.parseInt(st.nextToken());
-
-		dijkstra(start);
-		System.out.println(d[end]);
+		StringBuilder sb = new StringBuilder();
 		
-		int cnt = 0;
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		for(int i=0; i<=n; i++) {
+			graph.add(new ArrayList<Edge>());
+		}
+		
+		StringTokenizer st;
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken()); 
+			int to = Integer.parseInt(st.nextToken()); 
+			int cost = Integer.parseInt(st.nextToken()); 
+			graph.get(from).add(new Edge(to, cost));
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
+		
+		preCity = new int[n+1];
+		dijkstra(start, end);
+		sb.append(dist[end]).append("\n");
+		
 		Stack<Integer> stack = new Stack<>();
-		stack.push(end);
-		while (preCity[end] != 0) {
-			cnt += 1;
-			stack.push(preCity[end]);
-			end = preCity[end];
+		int index = end;
+		while(index != 0) {
+			stack.push(index);
+			index = preCity[index];
 		}
-		System.out.println(cnt + 1);
-		while (!stack.isEmpty()) {
-			System.out.print(stack.pop() + " ");
+		
+		sb.append(stack.size()).append("\n");
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop()).append(" ");
 		}
-
+		
+		System.out.println(sb);
+		
+		br.close();
 	}
-
-	private static void dijkstra(int start) {
-		PriorityQueue<City> pq = new PriorityQueue<City>();
-		pq.add(new City(start, 0));
-		d[start] = 0;
-
-		while (!pq.isEmpty()) {
-			City curCity = pq.poll();
-			int cur = curCity.to;
-			if (d[cur] < curCity.weight)
-				continue;
-			for (City next : graph.get(cur)) {
-				if (d[next.to] > d[cur] + next.weight) {
-					d[next.to] = d[cur] + next.weight;
-					preCity[next.to] = cur;
-					pq.offer(new City(next.to, d[next.to]));
+	
+	private static void dijkstra(int start, int end) {
+		boolean[] visited = new boolean[n+1];
+		dist = new int[n+1];
+		int INF = Integer.MAX_VALUE;
+		Arrays.fill(dist, INF);
+		dist[start] = 0;
+		
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.add(new Edge(start, 0));
+		while(!pq.isEmpty()) {
+			int curr = pq.poll().index;
+			
+			if(visited[curr]) continue;
+			visited[curr] = true;
+			
+			for(Edge next : graph.get(curr)) {
+				if(dist[next.index] > dist[curr] + next.cost) {
+					dist[next.index] = dist[curr] + next.cost;
+					pq.add(new Edge(next.index, dist[next.index]));
+					preCity[next.index] = curr;
 				}
 			}
 		}
