6b5eec5b4b7353a1303a603c2c41c6961ff9c5c3
imbeom35 <97426151+imbeom35@users.noreply.github.com>
2023-07-06 16:52:16 +0900

    [Bronze I] Title: 평균, Time: 144 ms, Memory: 16392 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        double[] arr = new double[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Double.parseDouble(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        double max = arr[N-1];
+        double sum = 0;
+
+        for(int i=0; i<N; i++) {
+            sum += arr[i]/max*100;
+        }
+
+        System.out.println(sum/N);
+
+        br.close();
+    }
+}
