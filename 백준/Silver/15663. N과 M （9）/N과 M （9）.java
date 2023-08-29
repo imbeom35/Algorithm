fed2629ea02ba497b50dcebb89a39e918aa49bb0
imbeom35 <S163306@365.mokpo.ac.kr>
2023-08-30 00:22:46 +0900

    [Silver II] Title: N과 M (9), Time: 636 ms, Memory: 34804 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static LinkedHashSet<String> ans;
+    static int N, M;
+    static int[] nums, perm;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        perm = new int[M];
+        visited = new boolean[N];
+        ans = new LinkedHashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        permutation(0);
+        ans.forEach(System.out::println);
+
+        br.close();
+    }
+
+    static void permutation(int cnt) {
+        if (cnt == M) {
+            StringBuilder sb = new StringBuilder();
+            for (int p : perm) {
+                sb.append(p).append(' ');
+            }
+            ans.add(sb.toString());
+            return;
+        }
+
+        for (int i=0; i<N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                perm[cnt] = nums[i];
+                permutation(cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
