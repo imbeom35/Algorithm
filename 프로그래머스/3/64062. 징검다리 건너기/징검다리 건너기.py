abf09c3a4b250730f9bfc43399cf3a82460192f7
imbeom35 <S163306@365.mokpo.ac.kr>
2024-07-18 08:00:44 +0900

    [level 3] Title: 징검다리 건너기, Time: 399.94 ms, Memory: 18.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,19 @@
+def solution(stones, k):
+    left = 1
+    right = max(stones) + 1
+    while left <= right:
+        mid = (left + right) // 2
+        cnt = 0
+        for stone in stones:
+            if stone - mid <= 0:
+                cnt += 1
+            else:
+                cnt = 0
+            if cnt >= k:
+                break
+        if cnt >= k:
+            right = mid - 1
+        else:
+            left = mid + 1
+            
+    return left
