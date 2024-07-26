8bc4398c5e71743b7e3bc4349dd15777f8cc98c9
imbeom35 <S163306@365.mokpo.ac.kr>
2024-07-26 18:55:17 +0900

    [level 3] Title: 블록 이동하기, Time: 2413.89 ms, Memory: 12.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
@@ -0,0 +1,51 @@
+from collections import deque
+
+def get_next_pos(pos, board):
+    next_pos = []
+    pos = list(pos) # 현재 위치 정보를 리스트로 변환 (집합 -> 리스트)
+    x1, y1, x2, y2 = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    for i in range(4):
+        nx1, ny1, nx2, ny2 = x1 + dx[i], y1 + dy[i], x2 + dx[i], y2 + dy[i]
+        if board[nx1][ny1] == 0 and board[nx2][ny2] == 0:
+            next_pos.append({(nx1, ny1), (nx2, ny2)})
+    
+    # 현재 로봇이 가로로 놓여 있는 경우
+    if x1 == x2:
+        for i in [-1, 1]:
+            if board[x1 + i][y1] == 0 and board[x2 + i][y2] == 0:
+                next_pos.append({(x1, y1), (x1 + i, y1)})
+                next_pos.append({(x2, y2), (x2 + i, y2)})
+                
+    # 현재 로봇이 세로로 놓여 있는 경우
+    elif y1 == y2:
+        for i in [-1, 1]:
+            if board[x1][y1 + i] == 0 and board[x2][y2 + i] == 0:
+                next_pos.append({(x1, y1), (x1, y1 + i)})
+                next_pos.append({(x2, y2), (x2, y2 + i)})
+                
+    return next_pos 
+
+def solution(board):
+    # 맵의 외곽에 벽을 두는 형태로 맵 변형
+    n = len(board)
+    new_board = [[1] * (n+2) for _ in range(n+2)]
+    for i in range(n):
+        for j in range(n):
+            new_board[i+1][j+1] = board[i][j]
+            
+    queue = deque()
+    visited= []
+    pos = {(1,1), (1,2)}
+    queue.append((pos,0))
+    visited.append(pos)
+    while queue:
+        pos, cost = queue.popleft()
+        if (n, n) in pos:
+            return cost
+        for next_pos in get_next_pos(pos, new_board):
+            if next_pos not in visited:
+                queue.append((next_pos, cost+1))
+                visited.append(next_pos)
+    return 0   
